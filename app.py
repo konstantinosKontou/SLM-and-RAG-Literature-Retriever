46e45449369e919fd458465c5ee0892a87fbba2a
Kontou31 <konstantinos.kontou@trinomial.xyz>
2025-07-09 15:06:24 +0300

    Add validation to check if the user gave question and pressed the button

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 import streamlit as st
 
 # SIDEBAR
@@ -9,3 +10,15 @@ top_k = st.sidebar.slider("Top K Chunks to Retrieve", 1, 10, 3)
 st.title("Local Academic RAG Assistant")
 st.markdown("Ask a question about your local academic PDFs.")
 user_question = st.text_input("Ask your question:")
+ask_button = st.button("Submit Question")
+
+if ask_button and user_question:
+
+    # CHECK IF THE PATH EXISTS
+    # IF NOT EXISTS
+    if not os.path.exists(pdf_folder):
+        st.error(f"Folder '{pdf_folder}' not found.")
+
+    # IF EXISTS
+    else:
+        st.success(f"Folder '{pdf_folder}' found.")
